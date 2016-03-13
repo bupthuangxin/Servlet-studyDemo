9ae7370cee3b8824a5671703b1444b11832d83bd
huangxin <308071066@qq.com>
2016-03-13 18:46:14 +0800

    转向

---
+++ requestForward/src/com/huangxin/Servlet1.java
@@ -0,0 +1,31 @@
+package com.huangxin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		String username=request.getParameter("username");
+		//把username放在request域对象中
+		request.setAttribute("u", username);
+		//使用转发的方法，把request, response对象传递给下一个servlet
+		request.getRequestDispatcher("/Servlet2").forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
---
+++ requestForward/src/com/huangxin/Servlet2.java
@@ -0,0 +1,29 @@
+package com.huangxin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet2 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String username=(String) request.getAttribute("u");
+		out.println(username);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
